e5fb6c898ad85cbba105ad2e1ac3cfdeb584c609
thien1407575 <36197665+thien1407575@users.noreply.github.com>
2023-10-19 14:52:59 +0700

    Update main.go

--- tools/lambda-promtail/lambda-promtail/main.go
+++ tools/lambda-promtail/lambda-promtail/main.go
@@ -200,145 +200,145 @@ func handler(ctx context.Context, ev map[string]interface{}) error {
 
 func main() {
 
-	os.Setenv("EXTRA_LABELS", "env,network,namespace,aws-service")
-	os.Setenv("LOG_LEVELLOG_LEVEL", "debug")
-	os.Setenv("USERNAME", "")
-	os.Setenv("OMIT_EXTRA_LABELS_PREFIX", "true")
-	os.Setenv("SKIP_TLS_VERIFY", "true")
-	os.Setenv("TENANT_ID", "torus-tenant")
-	os.Setenv("WRITE_ADDRESS", "http://localhost:8080/loki/api/v1/push")
+	// os.Setenv("EXTRA_LABELS", "env,network,namespace,aws-service")
+	// os.Setenv("LOG_LEVELLOG_LEVEL", "debug")
+	// os.Setenv("USERNAME", "")
+	// os.Setenv("OMIT_EXTRA_LABELS_PREFIX", "true")
+	// os.Setenv("SKIP_TLS_VERIFY", "true")
+	// os.Setenv("TENANT_ID", "torus-tenant")
+	// os.Setenv("WRITE_ADDRESS", "http://localhost:8080/loki/api/v1/push")
 
-	setupArguments()
+	// setupArguments()
 
-	evStr := `{
-		"body": "eyJ0ZXN0IjoiYm9keSJ9",
-		"resource": "/{proxy+}",
-		"path": "/path/to/resource",
-		"httpMethod": "POST",
-		"isBase64Encoded": true,
-		"queryStringParameters": {
-		  "foo": "bar"
-		},
-		"multiValueQueryStringParameters": {
-		  "foo": [
-			"bar"
-		  ]
-		},
-		"pathParameters": {
-		  "proxy": "/path/to/resource"
-		},
-		"stageVariables": {
-		  "baz": "qux"
-		},
-		"headers": {
-		  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-		  "Accept-Encoding": "gzip, deflate, sdch",
-		  "Accept-Language": "en-US,en;q=0.8",
-		  "Cache-Control": "max-age=0",
-		  "CloudFront-Forwarded-Proto": "https",
-		  "CloudFront-Is-Desktop-Viewer": "true",
-		  "CloudFront-Is-Mobile-Viewer": "false",
-		  "CloudFront-Is-SmartTV-Viewer": "false",
-		  "CloudFront-Is-Tablet-Viewer": "false",
-		  "CloudFront-Viewer-Country": "US",
-		  "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-		  "Upgrade-Insecure-Requests": "1",
-		  "User-Agent": "Custom User Agent String",
-		  "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-		  "X-Amz-Cf-Id": "cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA==",
-		  "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-		  "X-Forwarded-Port": "443",
-		  "X-Forwarded-Proto": "https"
-		},
-		"multiValueHeaders": {
-		  "Accept": [
-			"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
-		  ],
-		  "Accept-Encoding": [
-			"gzip, deflate, sdch"
-		  ],
-		  "Accept-Language": [
-			"en-US,en;q=0.8"
-		  ],
-		  "Cache-Control": [
-			"max-age=0"
-		  ],
-		  "CloudFront-Forwarded-Proto": [
-			"https"
-		  ],
-		  "CloudFront-Is-Desktop-Viewer": [
-			"true"
-		  ],
-		  "CloudFront-Is-Mobile-Viewer": [
-			"false"
-		  ],
-		  "CloudFront-Is-SmartTV-Viewer": [
-			"false"
-		  ],
-		  "CloudFront-Is-Tablet-Viewer": [
-			"false"
-		  ],
-		  "CloudFront-Viewer-Country": [
-			"US"
-		  ],
-		  "Host": [
-			"0123456789.execute-api.us-east-1.amazonaws.com"
-		  ],
-		  "Upgrade-Insecure-Requests": [
-			"1"
-		  ],
-		  "User-Agent": [
-			"Custom User Agent String"
-		  ],
-		  "Via": [
-			"1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)"
-		  ],
-		  "X-Amz-Cf-Id": [
-			"cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA=="
-		  ],
-		  "X-Forwarded-For": [
-			"127.0.0.1, 127.0.0.2"
-		  ],
-		  "X-Forwarded-Port": [
-			"443"
-		  ],
-		  "X-Forwarded-Proto": [
-			"https"
-		  ]
-		},
-		"requestContext": {
-		  "accountId": "123456789012",
-		  "resourceId": "123456",
-		  "stage": "prod",
-		  "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-		  "requestTime": "09/Apr/2015:12:34:56 +0000",
-		  "requestTimeEpoch": 1428582896000,
-		  "identity": {
-			"cognitoIdentityPoolId": null,
-			"accountId": null,
-			"cognitoIdentityId": null,
-			"caller": null,
-			"accessKey": null,
-			"sourceIp": "127.0.0.1",
-			"cognitoAuthenticationType": null,
-			"cognitoAuthenticationProvider": null,
-			"userArn": null,
-			"userAgent": "Custom User Agent String",
-			"user": null
-		  },
-		  "path": "/prod/path/to/resource",
-		  "resourcePath": "/{proxy+}",
-		  "httpMethod": "POST",
-		  "apiId": "1234567890",
-		  "protocol": "HTTP/1.1"
-		}
-	  }`
-	var ev map[string]interface{}
-	err := json.Unmarshal([]byte(evStr), &ev)
-	if err != nil {
-	}
-	ctx := context.Background()
-	handler(ctx, ev)
+	// evStr := `{
+	// 	"body": "eyJ0ZXN0IjoiYm9keSJ9",
+	// 	"resource": "/{proxy+}",
+	// 	"path": "/path/to/resource",
+	// 	"httpMethod": "POST",
+	// 	"isBase64Encoded": true,
+	// 	"queryStringParameters": {
+	// 	  "foo": "bar"
+	// 	},
+	// 	"multiValueQueryStringParameters": {
+	// 	  "foo": [
+	// 		"bar"
+	// 	  ]
+	// 	},
+	// 	"pathParameters": {
+	// 	  "proxy": "/path/to/resource"
+	// 	},
+	// 	"stageVariables": {
+	// 	  "baz": "qux"
+	// 	},
+	// 	"headers": {
+	// 	  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+	// 	  "Accept-Encoding": "gzip, deflate, sdch",
+	// 	  "Accept-Language": "en-US,en;q=0.8",
+	// 	  "Cache-Control": "max-age=0",
+	// 	  "CloudFront-Forwarded-Proto": "https",
+	// 	  "CloudFront-Is-Desktop-Viewer": "true",
+	// 	  "CloudFront-Is-Mobile-Viewer": "false",
+	// 	  "CloudFront-Is-SmartTV-Viewer": "false",
+	// 	  "CloudFront-Is-Tablet-Viewer": "false",
+	// 	  "CloudFront-Viewer-Country": "US",
+	// 	  "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+	// 	  "Upgrade-Insecure-Requests": "1",
+	// 	  "User-Agent": "Custom User Agent String",
+	// 	  "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+	// 	  "X-Amz-Cf-Id": "cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA==",
+	// 	  "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+	// 	  "X-Forwarded-Port": "443",
+	// 	  "X-Forwarded-Proto": "https"
+	// 	},
+	// 	"multiValueHeaders": {
+	// 	  "Accept": [
+	// 		"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+	// 	  ],
+	// 	  "Accept-Encoding": [
+	// 		"gzip, deflate, sdch"
+	// 	  ],
+	// 	  "Accept-Language": [
+	// 		"en-US,en;q=0.8"
+	// 	  ],
+	// 	  "Cache-Control": [
+	// 		"max-age=0"
+	// 	  ],
+	// 	  "CloudFront-Forwarded-Proto": [
+	// 		"https"
+	// 	  ],
+	// 	  "CloudFront-Is-Desktop-Viewer": [
+	// 		"true"
+	// 	  ],
+	// 	  "CloudFront-Is-Mobile-Viewer": [
+	// 		"false"
+	// 	  ],
+	// 	  "CloudFront-Is-SmartTV-Viewer": [
+	// 		"false"
+	// 	  ],
+	// 	  "CloudFront-Is-Tablet-Viewer": [
+	// 		"false"
+	// 	  ],
+	// 	  "CloudFront-Viewer-Country": [
+	// 		"US"
+	// 	  ],
+	// 	  "Host": [
+	// 		"0123456789.execute-api.us-east-1.amazonaws.com"
+	// 	  ],
+	// 	  "Upgrade-Insecure-Requests": [
+	// 		"1"
+	// 	  ],
+	// 	  "User-Agent": [
+	// 		"Custom User Agent String"
+	// 	  ],
+	// 	  "Via": [
+	// 		"1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)"
+	// 	  ],
+	// 	  "X-Amz-Cf-Id": [
+	// 		"cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA=="
+	// 	  ],
+	// 	  "X-Forwarded-For": [
+	// 		"127.0.0.1, 127.0.0.2"
+	// 	  ],
+	// 	  "X-Forwarded-Port": [
+	// 		"443"
+	// 	  ],
+	// 	  "X-Forwarded-Proto": [
+	// 		"https"
+	// 	  ]
+	// 	},
+	// 	"requestContext": {
+	// 	  "accountId": "123456789012",
+	// 	  "resourceId": "123456",
+	// 	  "stage": "prod",
+	// 	  "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+	// 	  "requestTime": "09/Apr/2015:12:34:56 +0000",
+	// 	  "requestTimeEpoch": 1428582896000,
+	// 	  "identity": {
+	// 		"cognitoIdentityPoolId": null,
+	// 		"accountId": null,
+	// 		"cognitoIdentityId": null,
+	// 		"caller": null,
+	// 		"accessKey": null,
+	// 		"sourceIp": "127.0.0.1",
+	// 		"cognitoAuthenticationType": null,
+	// 		"cognitoAuthenticationProvider": null,
+	// 		"userArn": null,
+	// 		"userAgent": "Custom User Agent String",
+	// 		"user": null
+	// 	  },
+	// 	  "path": "/prod/path/to/resource",
+	// 	  "resourcePath": "/{proxy+}",
+	// 	  "httpMethod": "POST",
+	// 	  "apiId": "1234567890",
+	// 	  "protocol": "HTTP/1.1"
+	// 	}
+	//   }`
+	// var ev map[string]interface{}
+	// err := json.Unmarshal([]byte(evStr), &ev)
+	// if err != nil {
+	// }
+	// ctx := context.Background()
+	// handler(ctx, ev)
 
-	// lambda.Start(handler)
+	lambda.Start(handler)
 }
